9564be326933b98a4d74d31ea207c77c288a802f
Alyona Gazizova <alyona.gazizova@strikersoft.com>
2018-10-19 11:52:34 +0300

    lists and keys

--- egghead/test-0918/src/pages/home.js
+++ egghead/test-0918/src/pages/home.js
@@ -1,12 +1,24 @@
 import React, {Component} from "react";
 
 export class HomeAlt extends Component{
-    
+    numbers = [1, 2, 3, 4, 5];
+
+    NumberList(props) {
+        const numbers = props.numbers;
+        const listItems = numbers.map((number) =>
+          <li key={number.toString()}>{number}</li>
+        );
+        return (
+          <ul>{listItems}</ul>
+        );
+      }
+
     render() {
         return (
             <div id="home">
                 <h2>This is the home page.</h2>
                 <div>home</div>
+                <this.NumberList numbers={this.numbers}/>
             </div>
         );
     }
