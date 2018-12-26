088212cff392c79e81243972ec7ac62500b3d465
Alyona Gazizova <alyona.gazizova@strikersoft.com>
2018-12-26 20:04:29 +0200

    js

--- src/App.js
+++ src/App.js
@@ -2,21 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-
-import Route from './routes'
+import Route from './routes';
 
 class App extends Component {
   render() {
+    // let popup = window.open('https://github.com/Gazizova', 'Github');
+    // popup.postMessage(
+    //   "The user is 'bob' and the password is 'secret'",
+    //   'https://secure.example.net'
+    // );
+    // popup.postMessage('hello there!', 'http://example.com');
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          </header>
-          <div>
+        </header>
+        <button onClick={() => window.open('https://github.com/Gazizova', 'Github')}>Button</button>
 
-          </div>
-          
-          <Route/>
+        <Route />
       </div>
     );
   }
