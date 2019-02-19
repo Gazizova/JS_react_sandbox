b14817691df4ab7ba81f90f12c1283b784d7c80b
Alyona Gazizova <alyona.gazizova@strikersoft.com>
2019-02-19 14:04:08 +0200

    finish AddTodo

--- src/courses/egghead/build_first_react_app/src/ToDoApp.js
+++ src/courses/egghead/build_first_react_app/src/ToDoApp.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
-import "./toDo.css";
-import { TodoForm, TodoList, Footer } from "./component/todo";
+import React, { Component } from 'react';
+import './toDo.css';
+import { TodoForm, TodoList, Footer } from './component/todo';
 import {
   addTodo,
   generalId,
@@ -9,24 +9,28 @@ import {
   updateTodo,
   removeTodo,
   filterTodos
-} from "./lib/todoHelper";
-import { ThemeContext } from "./component/router/Router";
-import { pipe, partial } from "./lib/utils";
+} from './lib/todoHelper';
+import { ThemeContext } from './component/router/Router';
+import { pipe, partial } from './lib/utils';
+import { loadTodos, createTodo, saveTodo, destroyTodo } from './lib/todoService';
 
 class ToDoApp extends Component {
   state = {
-    todos: [
-      { id: 1, name: "Learn JSX", isComplete: true },
-      { id: 2, name: "Build an Awesome App", isComplete: false },
-      { id: 3, name: "Ship It", isComplete: false }
-    ],
-    currentTodo: ""
+    todos: [],
+    currentTodo: ''
   };
+
+  static contextType = ThemeContext;
+
+  componentDidMount() {
+    loadTodos().then(todos => this.setState({ todos }));
+  }
 
   handleRemove = (id, evt) => {
     evt.preventDefault();
     const updatedTodos = removeTodo(this.state.todos, id);
     this.setState({ todos: updatedTodos });
+    destroyTodo(id).then(() => this.handleTempMessage('Todo removed'));
   };
 
   handleInputChange = event => {
@@ -46,16 +50,24 @@ class ToDoApp extends Component {
     const updatedTodos = addTodo(this.state.todos, newTodo);
     this.setState({
       todos: updatedTodos,
-      currentTodo: "",
-      errorMessage: ""
+      currentTodo: '',
+      errorMessage: ''
     });
+    createTodo(newTodo).then(this.handleTempMessage('Todo added'));
   };
+
   handleEmptySubmit = evt => {
     evt.preventDefault();
     this.setState({
-      errorMessage: "Please supply the todo name"
+      errorMessage: 'Please supply the todo name'
     });
   };
+
+  handleTempMessage = msg => {
+    this.setState({ successMessage: msg });
+    setTimeout(() => this.setState({ successMessage: '' }), 2000);
+  };
+
   handleToggle = id => {
     // const getupdatedTodos = pipe(
     //   findById,
@@ -63,24 +75,29 @@ class ToDoApp extends Component {
     //   partial(updateTodo, this.state.todos)
     // );
     // const updatedTodos = getupdatedTodos(id, this.state.todos);
+
+    // const todo = findById(id, this.state.todos);
+    // const toggled = toggleTodo(todo);
+    // const updatedTodos = updateTodo(this.state.todos, toggled);
+    // this.setState({ todos: updatedTodos });
+
     const todo = findById(id, this.state.todos);
     const toggled = toggleTodo(todo);
-    const updatedTodos = updateTodo(this.state.todos, toggled);
+    const getUpdatedTodos = partial(updateTodo, this.state.todos);
+    const updatedTodos = getUpdatedTodos(toggled);
     this.setState({ todos: updatedTodos });
+    saveTodo(toggled).then(() => this.handleTempMessage('Todo Updated'));
   };
 
-  static contextType = ThemeContext;
-
   render() {
-    const submitHandler = this.state.currentTodo
-      ? this.handleSubmit
-      : this.handleEmptySubmit;
+    const submitHandler = this.state.currentTodo ? this.handleSubmit : this.handleEmptySubmit;
 
     const displayTodos = filterTodos(this.state.todos, this.context.route);
     return (
       <div className="Todo-App">
-        {this.state.errorMessage && (
-          <div className="error">{this.state.errorMessage}</div>
+        {this.state.errorMessage && <div className="error">{this.state.errorMessage}</div>}
+        {this.state.successMessage && (
+          <div className="success lesson">{this.state.successMessage}</div>
         )}
         <TodoForm
           handleInputChange={this.handleInputChange}
